2a230fcf5da3abaa3bcaa15206a72dbe6c81417a
FFoell <florian_foell@yahoo.de>
2020-06-04 17:52:21 +0200

    Erstes Hochladen
    Erstmaliges Hochladen der Projektdateien und der Login-Seite

---
+++ HintergrundFrame.pas
@@ -0,0 +1,24 @@
+unit HintergrundFrame;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects;
+
+type
+  TFrame3 = class(TFrame)
+    Image1: TImage;
+    Image2: TImage;
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+implementation
+
+{$R *.fmx}
+
+end.
---
+++ Unit2.pas
@@ -0,0 +1,23 @@
+unit Unit2;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects;
+
+type
+  TFrame2 = class(TFrame)
+    Hintergrund: TImage;
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+implementation
+
+{$R *.fmx}
+
+end.
